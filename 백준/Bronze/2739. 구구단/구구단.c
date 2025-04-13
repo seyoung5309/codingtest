d53989773521ef395682fd6449b6d44ba108f9af
오세영 <leenight5309@naver.com>
2025-04-14 08:25:09 +0900

    [Bronze V] Title: 구구단, Time: 0 ms, Memory: 1112 KB -BaekjoonHub

---
+++ 백준/Bronze/2739. 구구단/구구단.c
@@ -0,0 +1,11 @@
+#define _CRT_SECURE_NO_WARNINGS
+#include <stdio.h>
+
+int main(void) {
+	int a;
+	scanf("%d", &a); 
+	for (int i = 1; i <= 9; i++) {
+		printf("%d * %d = %d\n", a, i, a * i);
+	}
+	return 0;
+} 
