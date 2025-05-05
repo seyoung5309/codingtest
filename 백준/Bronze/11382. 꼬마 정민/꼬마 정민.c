8c12025ac4a96856c9eb9bbb6273f8cf955eec32
오세영 <leenight5309@naver.com>
2025-05-05 23:37:49 +0900

    [Bronze V] Title: 꼬마 정민, Time: 0 ms, Memory: 1112 KB -BaekjoonHub

---
+++ 백준/Bronze/11382. 꼬마 정민/꼬마 정민.c
@@ -0,0 +1,12 @@
+#include <stdio.h>
+#define _CRT_SECURE_NO_WARNINGS
+
+int main(void) {
+	long long A, B, C;
+
+	scanf("%ld %lld %lld", &A, &B, &C);
+
+	printf("%lld", A + B + C); 
+
+	return 0;
+}
