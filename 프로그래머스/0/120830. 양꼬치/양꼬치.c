f4e473b5f149d5a3fe5e357af8b497ef7d505e0a
오세영 <leenight5309@naver.com>
2025-05-11 22:21:29 +0900

    [level 0] Title: 양꼬치, Time: 0.01 ms, Memory: 3.63 MB -BaekjoonHub

---
+++ 프로그래머스/0/120830. 양꼬치/양꼬치.c
@@ -0,0 +1,10 @@
+#include <stdio.h>
+#include <stdbool.h>
+#include <stdlib.h>
+
+int solution(int n, int k) {
+    int s = n/10;
+    int answer = 12000*n + 2000*k - 2000*s;
+    
+    return answer;
+}
