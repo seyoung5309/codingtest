1aa92499f18a9be3ff81c4c430782ec0644fc5a3
오세영 <leenight5309@naver.com>
2025-06-24 22:07:30 +0900

    [Bronze II] Title: 대표값2, Time: 184 ms, Memory: 17780 KB -BaekjoonHub

---
+++ 백준/Bronze/2587. 대표값2/대표값2.java
@@ -0,0 +1,17 @@
+import java.util.Scanner;
+import java.util.Arrays;
+
+public class Main {
+    public static void main(String[] args) {
+        Scanner scan = new Scanner(System.in);
+        int []arr = new int[5];
+        int sum = 0; 
+        for (int i = 0; i < 5; i++) {
+            arr[i] = scan.nextInt();
+            sum += arr[i];
+        }
+        Arrays.sort(arr);
+        System.out.println(sum/5);
+        System.out.println(arr[2]);
+    }
+}
