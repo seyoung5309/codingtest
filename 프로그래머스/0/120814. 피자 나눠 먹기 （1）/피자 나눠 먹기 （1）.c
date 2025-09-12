9232c38a52efd2ef938a0aa9e6141227245e9e74
오세영 <leenight5309@naver.com>
2025-09-13 00:58:41 +0900

    [level 0] Title: 피자 나눠 먹기 (1), Time: 0.01 ms, Memory: 4.44 MB -BaekjoonHub

---
+++ 프로그래머스/0/120814. 피자 나눠 먹기 （1）/피자 나눠 먹기 （1）.c
@@ -0,0 +1,22 @@
+#include <stdio.h>
+#include <stdbool.h>
+#include <stdlib.h>
+
+int solution(int n) {
+    int answer = 0;
+    int pizza = n/7;
+    switch (pizza) {
+        case 0:
+        case 1: 
+            answer = 1;
+            break;
+        default:
+            if (n%7 == 0) {
+                answer = pizza; 
+            } else {
+                answer = pizza+1;
+            }
+            break;
+    }
+    return answer;
+}
