29e6ee0560aff4052a0af632212eb0c48f283473
오세영 <leenight5309@naver.com>
2026-01-27 23:22:56 +0900

    [Bronze II] Title: 피보나치 수 5, Time: 0 ms, Memory: 1116 KB -BaekjoonHub

---
+++ 백준/Bronze/10870. 피보나치 수 5/피보나치 수 5.c
@@ -0,0 +1,16 @@
+#include <stdio.h>
+
+int f(int);
+
+int main(void) {
+    int n;
+    scanf("%d", &n);
+    printf("%d\n", f(n));
+    return 0;
+}
+
+int f(int n) {
+    if (n == 0) return 0;
+    if (n == 1) return 1;
+    return f(n - 1) + f(n - 2);
+}
