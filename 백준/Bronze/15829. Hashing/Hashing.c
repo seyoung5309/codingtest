05d1219b45631393bcd5f867d70477ee515d0c75
오세영 <leenight5309@naver.com>
2026-02-05 06:22:56 +0900

    [Bronze II] Title: Hashing, Time: 0 ms, Memory: 1112 KB, Score: 100 point -BaekjoonHub

--- 백준/Bronze/15829. Hashing/Hashing.c
+++ 백준/Bronze/15829. Hashing/Hashing.c
@@ -1,18 +1,19 @@
 #include <stdio.h>
 
 int main(void) {
-	int n;
-	scanf("%d", &n);
-	getchar();
-	
-	long long result = 0; long mul = 1;
-	for (int i = 1; i <= n; i++) {
-		char c;
-		scanf("%c", &c);
-		result += (c - 'a' + 1) * mul;
-		mul *= 31;
-	}
-    
-	printf("%d", result);
-	return 0; 
+    int n;
+    scanf("%d", &n);
+
+    const long long mod = 1234567891;
+    long long result = 0, mul = 1;
+
+    for (int i = 0; i < n; i++) {
+        char c;
+        scanf(" %c", &c);
+        result = (result + (c - 'a' + 1) * mul) % mod;
+        mul = (mul * 31) % mod;
+    }
+
+    printf("%lld", result);
+    return 0;
 }
