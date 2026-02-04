7282b084147862ec99079f86d70a12707a737f76
오세영 <leenight5309@naver.com>
2026-02-05 05:31:34 +0900

    [Bronze III] Title: 배수와 약수, Time: 0 ms, Memory: 1116 KB -BaekjoonHub

---
+++ 백준/Bronze/5086. 배수와 약수/배수와 약수.c
@@ -0,0 +1,14 @@
+#include <stdio.h>
+
+int main(void) {
+	while (1) {
+		int n, m;
+		scanf("%d %d", &n, &m);
+		if (n == 0 && m == 0) break;
+		if (m % n == 0) printf("factor\n");
+		else if (n % m == 0) printf("multiple\n");
+		else printf("neither\n");
+	}
+    
+	return 0; 
+}
