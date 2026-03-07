114d04d6fadfcffe2a518190d2f156dd20646ae3
오세영 <leenight5309@naver.com>
2026-03-08 00:36:29 +0900

    [Bronze I] Title: 소인수분해, Time: 20 ms, Memory: 1112 KB -BaekjoonHub

---
+++ 백준/Bronze/11653. 소인수분해/소인수분해.c
@@ -0,0 +1,18 @@
+#include <stdio.h>
+
+int main(void) {
+	int n;
+	scanf("%d", &n);
+	
+	while (n > 1) {
+		for (int i = 2; i <= n; i++) {
+			if (n % i == 0) {
+				printf("%d\n", i);
+				n /= i;
+				break;
+			}
+		}		
+	}
+	
+	return 0; 
+}
