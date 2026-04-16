feeb97ab4051832c74edf28b81600862403accc0
오세영 <leenight5309@naver.com>
2026-04-17 01:04:45 +0900

    [Silver III] Title: 1로 만들기, Time: 4 ms, Memory: 5016 KB -BaekjoonHub

---
+++ 백준/Silver/1463. 1로 만들기/1로 만들기.c
@@ -0,0 +1,26 @@
+#include <stdio.h>
+
+int dp[1000001];
+
+int min(int a, int b) {
+    return a < b ? a : b;
+}
+
+int main(void) {
+    int n;
+    scanf("%d", &n);
+
+    dp[1] = 0;
+
+    for (int i = 2; i <= n; i++) {
+        dp[i] = dp[i - 1] + 1;
+
+        if (i % 2 == 0)
+            dp[i] = min(dp[i], dp[i / 2] + 1);
+
+        if (i % 3 == 0)
+            dp[i] = min(dp[i], dp[i / 3] + 1);
+    }
+
+    printf("%d", dp[n]);
+}
