41aeb1427a1285d4e4be71d576ecb9c72077623a
오세영 <leenight5309@naver.com>
2026-05-05 03:17:56 +0900

    [level 0] Title: 문자열 계산하기, Time: 0.07 ms, Memory: 33.5 MB -BaekjoonHub

---
+++ 프로그래머스/0/120902. 문자열 계산하기/문자열 계산하기.js
@@ -0,0 +1,15 @@
+function solution(my_string) {
+    let arr = my_string.split(" ");
+    
+    let answer = Number(arr[0]);
+    
+    for (let i = 1; i < arr.length; i += 2) {
+        if (arr[i] === "+") {
+            answer += Number(arr[i + 1]);
+        } else {
+            answer -= Number(arr[i + 1]);
+        }
+    }
+    
+    return answer;
+}
